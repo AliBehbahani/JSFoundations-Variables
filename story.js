15548a92a55d6f790b1ef5bb5e3f57ddffef3978
Ali Behbahani <dr.ali.behbahani18@gmail.com>
2021-05-24 16:55:32 +0300

    variables practice

---
+++ story.js
@@ -0,0 +1,12 @@
+let name = "Laila"
+let age = 25
+let hobby = "Hoarding Books"
+
+console.log (`Mr./Mrs. ${name} , in 33 years, you will be ${age+ 33} years old. By then youll have decades of ${hobby} practice!`)
+
+
+
+
+
+
+
